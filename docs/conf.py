0eebabea713a33d58d6fbdba3769be1233bb410d
Ivan Golubnitchy <sos@team.abcd.bz>
2015-01-12 06:26:51 +0200

    del del del

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Вики minecraft хостинга'
-copyright = u'gamehost.abcd.bz | gamehost.io | gamehost.ovh'
+#copyright = u'gamehost.abcd.bz | gamehost.io | gamehost.ovh'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
